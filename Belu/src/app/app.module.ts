9f87b8bc694e60c104389ca3a4b6288d5c713f8c
BeluGenovese <belug1510@hotmail.com>
2023-05-15 12:24:04 -0300

    agrego bootstrap y comienzo front end

--- Belu/src/app/app.module.ts
+++ Belu/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgentinaComponent } from './components/logo-argentina/logo-argentina.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HysComponent } from './components/hys/hys.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgentinaComponent,
+    ExperienciaComponent,
+    BannerComponent,
+    AcercaComponent,
+    EducacionComponent,
+    FooterComponent,
+    HomeComponent,
+    HysComponent,
+    LoginComponent,
+    ProyectoComponent
   ],
   imports: [
     BrowserModule,
